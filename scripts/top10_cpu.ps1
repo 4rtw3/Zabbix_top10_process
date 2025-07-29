99df5cd9f9851aab2de258325527cd6245014a24
Artware <ando.andriamiharimanana@synerglass-soft.com>
2025-07-29 15:03:24 +0300

    Fix CPU value (float -> string)

--- scripts/top10_cpu.ps1
+++ scripts/top10_cpu.ps1
@@ -1,11 +1,11 @@
 $cores = (Get-WmiObject Win32_Processor).NumberOfLogicalProcessors
 while ($true) {
     $tmp = Get-WmiObject Win32_PerfFormattedData_PerfProc_Process | 
-    select-object -property Name, @{Name = "CPU"; Expression = {($_.PercentProcessorTime/$cores)}}, @{Name = "PID"; Expression = {$_.IDProcess}}, @{"Name" = "Memory(MB)"; Expression = {[int]($_.WorkingSetPrivate/1mb)}} |
+    select-object -property Name, @{Name = "CPU"; Expression = {([Math]::Round(($_.PercentProcessorTime/$cores),2)) -replace ',','.'}}, @{Name = "PID"; Expression = {$_.IDProcess}}, @{"Name" = "Memory(MB)"; Expression = {[int]($_.WorkingSetPrivate/1mb)}} |
     Where-Object {$_.Name -notmatch "^(idle|_total|system)$"} |
     Sort-Object -Property CPU -Descending|
     Select-Object -First 10;
     cls;
-    $tmp | Export-Csv -Path "C:\Program Files\Zabbix Agent\data\top10process.csv" -NoTypeInformation
+    $tmp | Export-Csv -Path "C:\Program Files\Zabbix Agent 2\data\top10process.csv" -NoTypeInformation
     Start-Sleep 30
 }
